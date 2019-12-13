f234d54f71ead9394af38be1fe82aba9b504ce15
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@localhost.localdomain>
2019-12-13 10:25:02 +0100

    Improved error handling for empty line in map

--- src/error_handling.c
+++ src/error_handling.c
@@ -7,9 +7,21 @@
 
 #include "my_sokoban.h"
 
+static int valid_nb_columns(map_t *map)
+{
+    int i = 0;
+
+    while (i < map->nb_lines) {
+        if (map->nb_columns[i] == 0)
+            return (0);
+        i += 1;
+    }
+    return (1);
+}
+
 int valid_map(map_t *map)
 {
-    if (!valid_characters(map))
+    if (!valid_characters(map) || !valid_nb_columns(map))
         return (free_map_and_returns(map, 0));
     if (!find_player(map) || !find_boxes(map) || !find_hole(map))
         return (free_map_and_returns(map, 0));
